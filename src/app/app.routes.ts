71f995c4b7e7097d470470639b7593c83ffc5aea
A.J <adhar_office@yahoo.com>
2025-04-05 12:58:20 -0600

    init

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { TestPageComponent } from './test-page/test-page.component'; // or your actual component
+
+export const routes: Routes = [
+  { path: 'test', component: TestPageComponent },
+];

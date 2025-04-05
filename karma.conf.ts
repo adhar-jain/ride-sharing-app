e5a1148a809351a99fbcbc63d5aced4b3a24c839
A.J <adhar_office@yahoo.com>
2025-04-05 14:08:30 -0600

    karma-2

--- karma.conf.ts
+++ karma.conf.ts
@@ -2,11 +2,13 @@ module.exports = function (config: {
         set: (arg0: {
             browsers: string[]; browserDisconnectTimeout: number; // Wait 2 seconds before disconnect
             browserNoActivityTimeout: number;
+            singleRun: boolean;
         }) => void;
     }) {
     config.set({
       browsers: ['ChromeHeadless'],
       browserDisconnectTimeout: 2000, // Wait 2 seconds before disconnect
       browserNoActivityTimeout: 10000, // Wait 10 seconds for inactivity
+      singleRun: true,
     });
   };

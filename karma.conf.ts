37164f460e83253b11f3107e4aa06443af7b3cda
A.J <adhar_office@yahoo.com>
2025-04-09 20:45:26 -0600

    chore: update @angular-devkit/build-angular to 19.2.7 and karma to 6.4.2

--- karma.conf.ts
+++ karma.conf.ts
@@ -3,6 +3,7 @@ module.exports = function (config: {
             browsers: string[]; browserDisconnectTimeout: number; // Wait 2 seconds before disconnect
             browserNoActivityTimeout: number;
             singleRun: boolean;
+            plugins?: any[]; // Add plugins property to the type definition
         }) => void;
     }) {
     config.set({
@@ -10,5 +11,12 @@ module.exports = function (config: {
       browserDisconnectTimeout: 2000, // Wait 2 seconds before disconnect
       browserNoActivityTimeout: 10000, // Wait 10 seconds for inactivity
       singleRun: true,
-    });
+      plugins: [
+        require('karma-chrome-launcher'),
+        require('karma-jasmine'),
+        require('karma-jasmine-html-reporter'),
+        require('@angular-devkit/build-angular/plugins/karma')
+      ]
+    });      
+    
   };
